72f9496e72fbc2013472413f7fc4f624a84b2088
boxfish <boxfish893@gmail.com>
2024-03-30 01:21:35 +0900

    feat: コメントを受け取る処理完成

--- hub/src/index.ts
+++ hub/src/index.ts
@@ -1,6 +1,35 @@
-process.stdin.resume();
+import main from "./main";
+
 process.stdin.setEncoding("utf8");
 
-process.stdin.on("data", function (chunk: string) {
-  console.log(chunk);
+let nicoComeNum = 0;
+process.stdin.on("data", async (chunk: string) => {
+  let comment = [];
+  if (chunk.startsWith("{")) {
+    const styleChunk = "[" + chunk.trim().slice(0, -1) + "]";
+    const data = JSON.parse(styleChunk) as {
+      thread: string;
+      no: number;
+      vpos: number;
+      date: number;
+      mail: string;
+      user_id: string;
+      premium: number;
+      anonymity: number;
+      content: string;
+    }[];
+    if (data.length > 0) {
+      for (let d of data) {
+        if (d.content && d.no && d.no > nicoComeNum) {
+          comment.push(d.content);
+          nicoComeNum = d.no;
+        }
+      }
+    }
+  } else {
+    comment.push(chunk.split("content:")[1]);
+  }
+  for (let comme of comment) {
+    main(comme);
+  }
 });
