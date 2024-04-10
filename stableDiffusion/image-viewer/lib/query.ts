1f31efaa21b409c50d5416629e7a215f99f727ef
boxfish <boxfish893@gmail.com>
2024-04-11 02:29:23 +0900

    fix: image-viewerで最新の画像が表示されない問題を修正

--- stableDiffusion/image-viewer/lib/query.ts
+++ stableDiffusion/image-viewer/lib/query.ts
@@ -1,5 +1,10 @@
 const query = async (limit: number, offset: number) => {
-  const url = process.env.ENDPOINT + `/id?limit=${limit}&offset=${offset}`;
+  const url =
+    process.env.ENDPOINT +
+    `/id?iaddtime=${new Date(
+      new Date().getTime() - 1000 * 60 * 60 * 24
+    ).toLocaleString("sv-SE")}`;
+  console.log(url);
   const res = await fetch(url, { cache: "no-cache" });
   const result = (await res.json()) as {
     results: { id: number; iaddtime: string; path: string }[];
