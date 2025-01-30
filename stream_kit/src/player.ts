c15db81946c3da91fc39fafd7187b8124d6bc05e
boxfish_jp <boxfish893@gmail.com>
2025-01-30 21:46:34 +0900

    fix: コメントの読み上げ順番が崩れる問題を修正

--- stream_kit/src/player.ts
+++ stream_kit/src/player.ts
@@ -3,22 +3,26 @@ import type { Comment } from "./comment";
 
 export class Player {
 	private _queue: Comment[] = [];
+	private isProcessing = false;
 
 	addQueue(comment: Comment) {
 		this._queue.push(comment);
-		if (this._queue.length === 1) {
-			this.play();
+		if (this._queue.length && !this.isProcessing) {
+			this._play();
 		}
 	}
 
-	private async play() {
+	private async _play() {
+		this.isProcessing = true;
 		const comment = this._queue[0];
 		const audio = new AudioHandler(comment.content);
 		const wavData = await audio.getVoicePeakData();
 		await audio.play(wavData);
 		this._queue.shift();
-		if (this._queue.length > 0) {
-			this.play();
+		if (this._queue.length === 0) {
+			this.isProcessing = false;
+			return;
 		}
+		this._play();
 	}
 }
