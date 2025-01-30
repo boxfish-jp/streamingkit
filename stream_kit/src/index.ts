dc4ea66728333d281925def5badfa68235e6526f
boxfish_jp <boxfish893@gmail.com>
2025-01-31 00:54:43 +0900

    feat: socketioサーバーを追加

--- stream_kit/src/index.ts
+++ stream_kit/src/index.ts
@@ -1,5 +1,7 @@
+import type { Server as HttpServer } from "node:http";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { Server as SocketServer } from "socket.io";
 import { niconico } from "./get_comment";
 import { Player } from "./player";
 
@@ -9,15 +11,30 @@ app.get("/", (c) => {
 	return c.text("Hello Hono!");
 });
 
-const port = 3000;
+export const port = 3000;
+export const hostname = "localhost";
 console.log(`Server is running on http://localhost:${port}`);
 
+const server = serve({ fetch: app.fetch, port: port, hostname: hostname });
+
+export const ioServer = new SocketServer(server as HttpServer, {
+	path: "/ws",
+	serveClient: false,
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	},
+});
+
+ioServer.on("error", (err) => {
+	console.log(err);
+});
+
 const player = new Player();
-niconico("lv346903029", async (comment) => {
+niconico("lv346910090", async (comment) => {
 	player.addQueue(comment);
 });
 
-serve({
-	fetch: app.fetch,
-	port,
-});
+setInterval(() => {
+	ioServer.emit("play", "にゃんぱすー");
+}, 10000);
