0d8d8057b216162f0f0303399af426dd5d06bf6a
boxfish_jp <boxfish893@gmail.com>
2025-01-31 22:06:35 +0900

    feat: コメントのフィルタリングを追加

--- stream_kit/src/comment.ts
+++ stream_kit/src/comment.ts
@@ -1,6 +1,23 @@
+import { getEducationData } from "./lib/education";
+
 export class Comment {
 	content = "";
 	constructor(content: string) {
 		this.content = content;
+	}
+
+	fillter() {
+		if (this.content.search(/https?:\/\//) !== -1) {
+			this.content = this.content.replace(
+				/https?:\/\/[\w!?/\+\-_~=;\.,*&@#$%\(\)\'\[\]]+(\?[a-zA-Z0-9%=\-&_]+)?/g,
+				"URL省略",
+			);
+		}
+		const eduData = getEducationData();
+		for (const key in eduData) {
+			if (this.content.indexOf(key) !== -1) {
+				this.content = this.content.replace(new RegExp(key, "g"), eduData[key]);
+			}
+		}
 	}
 }
