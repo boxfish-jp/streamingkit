506c67adeb295d2c2d6a47b4bd800445c25ad7b0
boxfish_jp <boxfish893@gmail.com>
2025-02-14 20:42:07 +0900

    fix: 音声合成部分のエラー処理を変更

--- stream_kit/src/player.ts
+++ stream_kit/src/player.ts
@@ -2,33 +2,34 @@ import { AudioHandler } from "./audio";
 import type { Comment } from "./comment";
 
 export class Player {
-  private _queue: Comment[] = [];
-  private isProcessing = false;
+	private _queue: Comment[] = [];
+	private isProcessing = false;
 
-  addQueue(comment: Comment) {
-    this._queue.push(comment);
-    if (this._queue.length && !this.isProcessing) {
-      this._play();
-    }
-  }
+	addQueue(comment: Comment) {
+		this._queue.push(comment);
+		if (!this.isProcessing) {
+			this._play();
+		}
+	}
 
-  private async _play() {
-    this.isProcessing = true;
-    const comment = this._queue.shift();
-    if (comment === undefined) {
-      return;
-    }
-    const audio = new AudioHandler(comment.content);
-    const wavData = await audio.getVoicePeakData();
-    if (wavData === undefined) {
-      return;
-    }
-    await audio.play(wavData);
-
-    if (this._queue.length === 0) {
-      this.isProcessing = false;
-      return;
-    }
-    this._play();
-  }
+	private async _play() {
+		try {
+			this.isProcessing = true;
+			while (true) {
+				const comment = this._queue[0];
+				if (comment === undefined) {
+					break;
+				}
+				const audio = new AudioHandler(comment.content);
+				const wavData = await audio.getVoicePeakData();
+				if (wavData === undefined) {
+					continue;
+				}
+				this._queue.shift();
+				await audio.play(wavData);
+			}
+		} finally {
+			this.isProcessing = false;
+		}
+	}
 }
