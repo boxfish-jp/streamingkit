7622a51c3dbd72c0638a39011dca69767c421f54
boxfish_jp <boxfish893@gmail.com>
2025-02-15 23:19:34 +0900

    fix: urlも正規化されて問題の修正と教育が適用されない問題の修正

--- stream_kit/src/comment.ts
+++ stream_kit/src/comment.ts
@@ -8,7 +8,7 @@ export class Comment {
 	getEducatiedComment() {
 		let text = this.content;
 		if (this.content.search(/https?:\/\//) !== -1) {
-			text = this.content.replace(
+			text = text.replace(
 				/https?:\/\/[\w!?/\+\-_~=;\.,*&@#$%\(\)\'\[\]]+(\?[a-zA-Z0-9%=\-&_]+)?/g,
 				"URL省略",
 			);
@@ -16,7 +16,7 @@ export class Comment {
 		const eduData = getEducationData();
 		for (const key in eduData) {
 			if (this.content.indexOf(key) !== -1) {
-				text = this.content.replace(new RegExp(key, "g"), eduData[key]);
+				text = text.replace(new RegExp(key, "g"), eduData[key]);
 			}
 		}
 		console.log("text", text);
@@ -24,6 +24,9 @@ export class Comment {
 	}
 
 	fillter() {
+		if (this.content.search(/https?:\/\//) !== -1) {
+			return;
+		}
 		this.content = this.content.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (char) =>
 			String.fromCharCode(char.charCodeAt(0) - 0xfee0),
 		);
