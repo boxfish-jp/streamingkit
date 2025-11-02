d63f29087352bb57c29d6e2aecaaf9d0c1f638ce
boxfish_jp <79849824+boxfish-jp@users.noreply.github.com>
2025-11-02 23:44:34 +0900

    feat: 更地にした

--- effect/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
-
-// https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tailwindcss(), viteSingleFile()],
-});
