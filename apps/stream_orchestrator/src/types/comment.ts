28de6deb91b6482687b18af4baa22f80b7cc05d8
boxfish_jp <79849824+boxfish-jp@users.noreply.github.com>
2025-12-07 00:32:17 +0900

    feat: commentからフィルター済みのコンテンツを取得できるように

--- apps/stream_orchestrator/src/types/comment.ts
+++ apps/stream_orchestrator/src/types/comment.ts
@@ -1,3 +1,5 @@
+import { urlRegex } from "../url_regex.js";
+
 type CommentLabel = "viewer" | "bot" | "fuguo";
 
 export type NotifyCommentCallback = (message: CommentMessage) => void;
@@ -9,7 +11,6 @@ export class CommentMessage {
   readonly rawUserId?: bigint;
   readonly hashedUserId?: string;
   readonly content: string;
-  speekText: string;
 
   constructor(
     label: CommentLabel,
@@ -20,9 +21,27 @@ export class CommentMessage {
   ) {
     this.label = label;
     this.content = content;
-    this.speekText = content;
     this.username = username;
     this.rawUserId = rawUserId;
     this.hashedUserId = hashedUserId;
+  }
+
+  get filteredContent(): string {
+    const textParts = this.content.split(urlRegex);
+    const urlParts = this.content.match(urlRegex);
+
+    let text = "";
+    for (const part of textParts) {
+      if (part === undefined) {
+        text += urlParts?.shift() ?? "";
+      } else if (part) {
+        text += part
+          .replace(/[Ａ-Ｚａ-ｚ０-９]/g, (char) =>
+            String.fromCharCode(char.charCodeAt(0) - 0xfee0),
+          )
+          .replace(/[A-Za-z]/g, (char) => char.toLowerCase());
+      }
+    }
+    return text;
   }
 }
---
+++ apps/stream_orchestrator/test/comment_message.test.ts
@@ -0,0 +1,40 @@
+import { describe, expect, test } from "vitest";
+import { CommentMessage } from "../src/types/comment.js";
+
+describe("commentMessage: filter", () => {
+  test("変換対象がない文字列はそのまま出力", () => {
+    const message: CommentMessage = new CommentMessage("viewer", "hoge");
+    expect(message.filteredContent).toBe("hoge");
+  });
+
+  test("変換対象(全角数字)は半角数字に変換", () => {
+    const message: CommentMessage = new CommentMessage(
+      "viewer",
+      "１２３４５６７８９０",
+    );
+    expect(message.filteredContent).toBe("1234567890");
+  });
+
+  test("変換対象(大文字アルファベット)は小文字アルファベットに変換", () => {
+    const message: CommentMessage = new CommentMessage("viewer", "OMG");
+    expect(message.filteredContent).toBe("omg");
+  });
+
+  test("変換対象が含まれたURLはそのまま出力", () => {
+    const message: CommentMessage = new CommentMessage(
+      "viewer",
+      "https://www.youtube.com/watch?v=JwWVgGE5b0Q",
+    );
+    expect(message.filteredContent).toBe(
+      "https://www.youtube.com/watch?v=JwWVgGE5b0Q",
+    );
+  });
+
+  test("URLの中に変換対象(全角アルファベット)とその外に変換対象(全角アルファベット)は、URLはそのままで変換対象は小文字にする。", () => {
+    const message: CommentMessage = new CommentMessage(
+      "viewer",
+      "https://ｚｄｋ.com ｚｄｋ",
+    );
+    expect(message.filteredContent).toBe("https://ｚｄｋ.com zdk");
+  });
+});
