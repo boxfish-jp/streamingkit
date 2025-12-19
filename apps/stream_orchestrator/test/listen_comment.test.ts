5843d194606d945b3cbfbc4e883268cc48ee376c
boxfish_jp <79849824+boxfish-jp@users.noreply.github.com>
2025-12-19 23:04:54 +0900

    test: listen_commentのテストコードが不安定な問題を修正

--- apps/stream_orchestrator/test/listen_comment.test.ts
+++ apps/stream_orchestrator/test/listen_comment.test.ts
@@ -1,16 +1,16 @@
 import type { ErrorMessage } from "kit_models";
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { ListenComment } from "../src/listen_comment.js";
 
-test("listenComent: invalid Id", async () => {
-  const listenComment = new ListenComment();
-  const callback = (errorMessage: ErrorMessage) => {
-    expect(errorMessage.message).toBe(
-      'SyntaxError: "undefined" is not valid JSON',
-    );
-  };
-  listenComment.on("error", callback);
-  listenComment.start("0");
-  // 200ms待つ
-  await new Promise((resolve) => setTimeout(resolve, 200));
+describe("listenComment", () => {
+  test("無効な生放送IDを入れると、serverWatchCommentでのエラーが出る", async () => {
+    const listenComment = new ListenComment();
+    const callback = (errorMessage: ErrorMessage) => {
+      expect(errorMessage.status).toBe("serverWatchComment");
+    };
+    listenComment.on("error", callback);
+    listenComment.start("0");
+    // 200ms待つ
+    await new Promise((resolve) => setTimeout(resolve, 200));
+  });
 });
