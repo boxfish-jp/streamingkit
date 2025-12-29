f5288a5ff14d5754d2b8e999c8ea20475cc3bf31
boxfish_jp <79849824+boxfish-jp@users.noreply.github.com>
2025-12-30 00:34:23 +0900

    feat: フロントエンドからオーケストレーターのURLを指定できるようにした

--- apps/desktop_cilent/src/renderer/src/lib/socket.ts
+++ apps/desktop_cilent/src/renderer/src/lib/socket.ts
@@ -1,40 +1,89 @@
 import type { Message } from "kit_models";
-import { io } from "socket.io-client";
+import { io, type Socket } from "socket.io-client";
 import customParser from "socket.io-msgpack-parser";
 
-export const connectSocket = (emitMessage: (message: Message) => void) => {
-  const socket = io("http://localhost:8888", {
-    path: "/ws",
-    parser: customParser,
-  });
+export class SocketManager {
+  private _serverUrl: string;
+  private static _instance: SocketManager;
+  private _onMessageCallbacks: Array<(message: Message) => void> = [];
+  private _socket: Socket;
 
-  socket.on("connect", async () => {
-    console.log("Socket connected:", socket.id);
-    emitMessage({
-      type: "notify",
-      status: "clientSocketConnected",
+  private constructor() {
+    this._serverUrl = this._readUrlFromStorage();
+  }
+
+  get serverUrl(): string {
+    return this._serverUrl;
+  }
+
+  public setServerUrl(url: string) {
+    this._saveUrlToStorage(url);
+    this._serverUrl = url;
+    this._socket.close();
+    this.connect();
+  }
+
+  private _readUrlFromStorage(): string {
+    const item = localStorage.getItem("orchestratorUrl");
+    console.log("Read orchestratorUrl from storage:", item);
+    return item ?? "http://localhost:8888";
+  }
+
+  private _saveUrlToStorage(url: string) {
+    localStorage.setItem("orchestratorUrl", url);
+    console.log("Saved orchestratorUrl to storage:", url);
+  }
+  private _emit(message: Message) {
+    this._onMessageCallbacks.forEach((callback) => {
+      callback(message);
     });
-  });
+  }
 
-  socket.on("message", async (message: Message) => {
-    console.log("Received message:", message);
-    emitMessage(message);
-  });
+  public on(callback: (message: Message) => void) {
+    this._onMessageCallbacks.push(callback);
+  }
 
-  socket.on("disconnect", async () => {
-    emitMessage({
-      type: "error",
-      status: "clientSocketDisconnected",
-      time: Date.now(),
+  static instance(): SocketManager {
+    if (!SocketManager._instance) {
+      SocketManager._instance = new SocketManager();
+    }
+    return SocketManager._instance;
+  }
+
+  public connect() {
+    this._socket = io(this._serverUrl, {
+      path: "/ws",
+      parser: customParser,
     });
-  });
 
-  socket.on("connect_error", async (err) => {
-    emitMessage({
-      type: "error",
-      status: "clientSocketConnection",
-      time: Date.now(),
+    this._socket.on("connect", async () => {
+      console.log("Socket connected:", this._socket.id);
+      this._emit({
+        type: "notify",
+        status: "clientSocketConnected",
+      });
     });
-    console.log("接続エラーが発生しました:", err.message);
-  });
-};
+
+    this._socket.on("message", async (message: Message) => {
+      console.log("Received message:", message);
+      this._emit(message);
+    });
+
+    this._socket.on("disconnect", async () => {
+      this._emit({
+        type: "error",
+        status: "clientSocketDisconnected",
+        time: Date.now(),
+      });
+    });
+
+    this._socket.on("connect_error", async (err) => {
+      this._emit({
+        type: "error",
+        status: "clientSocketConnection",
+        time: Date.now(),
+      });
+      console.log("接続エラーが発生しました:", err.message);
+    });
+  }
+}
