e2c4427ebc4035ee94abb9ec87c4da30885a1d2f
boxfish_jp <79849824+boxfish-jp@users.noreply.github.com>
2025-12-31 01:19:24 +0900

    feat: 動画コマンドのワイルドカードを追加 / #44

--- apps/stream_orchestrator/src/command/video_command.ts
+++ apps/stream_orchestrator/src/command/video_command.ts
@@ -19,6 +19,7 @@ export const getVideoCommands = async () => {
       }
     });
   });
+
   const commands: Command[] = [];
   for (const keyword of keywords) {
     commands.push({
@@ -43,5 +44,86 @@ export const getVideoCommands = async () => {
       },
     });
   }
-  return commands;
+
+  const explosion = {
+    isTarget: (comment) =>
+      normalizeLowerCase(comment.content).startsWith("エクスプロージョン"),
+    synthesize: (comment: CommentMessage) => {
+      if (
+        normalizeLowerCase(comment.content).length > "エクスプロージョン".length
+      ) {
+        return {
+          type: "instSynthesize",
+          content: normalizeLowerCase(comment.content),
+          tag: "comment",
+        } as InstSyntesizeMessage;
+      }
+    },
+    action: () => {
+      const explosionNumber = Math.floor(Math.random() * 4) + 1;
+      return [
+        {
+          type: "video",
+          name: `エクスプロージョン${explosionNumber}`,
+        } as VideoMessage,
+      ];
+    },
+  } as Command;
+
+  commands.push(makeWildCardVideoCommand("?", "？"));
+  commands.push(makeWildCardVideoCommand("8888", "８８８８"));
+  commands.push(makeWildCardVideoCommand("５６す", "56す"));
+  commands.push(makeWildCardVideoCommand("にっこにこに", "にっこにこにー"));
+  commands.push(makeWildCardVideoCommand("にゃんぱす", "にゃんぱすー"));
+  commands.push(makeWildCardVideoCommand("やっはろー", "やっはろ"));
+  commands.push(makeWildCardVideoCommand("トゥットゥル", "トゥットゥルー"));
+  commands.push(makeWildCardVideoCommand("馬鹿", "バカ"));
+  commands.push(makeWildCardVideoCommand("ばか", "バカ"));
+  commands.push(
+    makeWildCardVideoCommand("これでかったと", "これで勝ったと思うなよ"),
+  );
+  commands.push(
+    makeWildCardVideoCommand("これで勝ったと", "これで勝ったと思うなよ"),
+  );
+  commands.push(makeWildCardVideoCommand("無理", "ムリ"));
+  commands.push(makeWildCardVideoCommand("むり", "ムリ"));
+  commands.push(makeWildCardVideoCommand("かんぺき", "完璧"));
+  commands.push(makeWildCardVideoCommand("幸せなら", "幸せならok"));
+  commands.push(makeWildCardVideoCommand("しあわせなら", "幸せならok"));
+  commands.push(makeWildCardVideoCommand("幸せならおｋ", "幸せならok"));
+  commands.push(makeWildCardVideoCommand("気になります", "私気になります"));
+  commands.push(makeWildCardVideoCommand("笑えばいい", "笑えばいいと思うよ"));
+  commands.push(makeWildCardVideoCommand("ちょわよ", "チョワヨー"));
+  commands.push(makeWildCardVideoCommand("ちょわよー", "チョワヨー"));
+  commands.push(makeWildCardVideoCommand("チョワヨ", "チョワヨー"));
+  commands.push(makeWildCardVideoCommand("いいね", "チョワヨー"));
+  commands.push(makeWildCardVideoCommand("スキピ", "スピキ"));
+  commands.push(makeWildCardVideoCommand("ｽﾋｷ", "スピキ"));
+  commands.push(makeWildCardVideoCommand("すぴき", "スピキ"));
+
+  return [...commands, explosion];
+};
+
+const makeWildCardVideoCommand = (wildcard: string, videoName: string) => {
+  return {
+    isTarget: (comment) =>
+      normalizeLowerCase(comment.content).startsWith(wildcard),
+    synthesize: (comment: CommentMessage) => {
+      if (normalizeLowerCase(comment.content).length > wildcard.length) {
+        return {
+          type: "instSynthesize",
+          content: normalizeLowerCase(comment.content),
+          tag: "comment",
+        } as InstSyntesizeMessage;
+      }
+    },
+    action: () => {
+      return [
+        {
+          type: "video",
+          name: videoName,
+        } as VideoMessage,
+      ];
+    },
+  } as Command;
 };
