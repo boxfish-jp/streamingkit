b78a92ebc4778d81e05894b4411d449bb9903e58
boxfish_jp <79849824+boxfish-jp@users.noreply.github.com>
2026-02-23 17:07:00 +0900

    test: hubのテストコードをさらに追加 / #62

--- apps/hub/test/index.test.ts
+++ apps/hub/test/index.test.ts
@@ -5,12 +5,11 @@ import { main } from "../src/index.ts";
 
 const host = "0.0.0.0";
 const port = 5000;
+const name = `http://${host}:${port}`;
 main(host, port);
 
 describe("hub:", () => {
   test("クライアントが接続する", async () => {
-    const name = `http://${host}:${port}`;
-    console.log(name);
     const socket = io(name, {
       path: "/ws",
       parser: customParser,
@@ -19,6 +18,60 @@ describe("hub:", () => {
       socket.on("connect", () => {
         socket.disconnect();
         resolve();
+      });
+    });
+  });
+
+  test("クライアントが送信したメッセージはブロードキャストされる", async () => {
+    const clientA = io(name, {
+      path: "/ws",
+      parser: customParser,
+    });
+
+    const clientB = io(name, {
+      path: "/ws",
+      parser: customParser,
+    });
+
+    await new Promise<void>((resolve) => {
+      clientA.on("connect", () => {
+        clientA.emit("message", "hello");
+      });
+      clientB.on("message", (message) => {
+        expect(message).toBe("hello");
+        clientA.disconnect();
+        clientB.disconnect();
+        resolve();
+      });
+    });
+  });
+
+  test("クライアントが送信したメッセージはブロードキャストされるが、自分が送ったメッセージは返ってこない", async () => {
+    const clientA = io(name, {
+      path: "/ws",
+      parser: customParser,
+    });
+
+    const clientB = io(name, {
+      path: "/ws",
+      parser: customParser,
+    });
+
+    await new Promise<void>((resolve) => {
+      clientA.on("connect", () => {
+        clientA.emit("message", "hello");
+      });
+
+      clientA.on("message", (message) => {
+        if (!message) return;
+        expect(message).toEqual("hello from B");
+        clientA.disconnect();
+        clientB.disconnect();
+        resolve();
+      });
+
+      clientB.on("message", (message) => {
+        clientB.emit("message", "hello from B");
       });
     });
   });
