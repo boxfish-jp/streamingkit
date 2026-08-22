be6380309096835d90a431c08c7876765de04972
boxfish_jp <79849824+boxfish-jp@users.noreply.github.com>
2026-08-22 15:51:41 +0900

    fix: flake.nixを更新

--- flake.nix
+++ flake.nix
@@ -19,16 +19,16 @@
         pkgs = import nixpkgs { inherit system; };
         pnpm = pkgs.pnpm_11;
         node = pkgs.nodejs_24;
-        version = "v1.0.15";
+        version = "v1.0.17";
         desktopSrc = fetchTarball {
           url = "https://github.com/boxfish-jp/streamingkit/releases/download/${version}/app-ubuntu-latest.tar.gz";
-          sha256 = "sha256:07vnvb1yfygf6rwfg7i936dpbb7afff2isrmgrdc4dpqj7ais8mf";
+          sha256 = "sha256:0qmkp5mzv79v99m8xj2h3m594z3h3b3nd70pq4n20zy77lc2n2di";
         };
         src = pkgs.fetchFromGitHub {
           owner = "boxfish-jp";
           repo = "streamingkit";
           rev = version;
-          hash = "sha256-oxxUH/y+MYz2ARxoz59OfqU9A/hWfVrY1cUuNtGHxC8=";
+          hash = "sha256-OVW+L6M+kEKVidgBwN6+41giLNC6Sl/PDNKdec1TdRc=";
         };
         pnpmDeps = pkgs.fetchPnpmDeps {
           pname = "streamingkit";
